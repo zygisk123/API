45e064192cf3aafd5ab5b89f01759406c13cd1eb
zygisk123 <zygimantask13@gmail.com>
2022-10-26 13:00:26 +0300

    iskelia pav. ir img.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
+var pokeName = document.getElementById("pokeName");
+let pokeImg = document.getElementById("img");
+let btn = document.getElementById('btnClick');
 
-async function getData() {
-    let response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+let randomID;
+
+
+async function getPokemon() {
+    randomID =  Math.floor(Math.random() * 500);
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon/' + randomID);
     let data = await response.json();
-    return data;
+    console.log(data);
+    pokeName.innerHTML = data.name;
+    pokeImg.src = data.sprites.front_default;
+
 }
-getData().then(data => console.log(data));
+
+getPokemon()
+// getData().then(data => pokeName.innerHTML = data.name);
+
+// getData().then(data => pokeImg.src = data.sprites.front_default);
+
